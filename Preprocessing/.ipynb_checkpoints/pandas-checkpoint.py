f4908d12a7ca413e07f2fb682108b35ac60a11ed
dsuke <daisuke0582@gmail.com>
2021-03-11 21:10:18 +0900

    add new folders

--- Preprocessing/.ipynb_checkpoints/pandas-checkpoint.py
+++ Preprocessing/.ipynb_checkpoints/pandas-checkpoint.py
@@ -4,6 +4,7 @@ import pandas as pd
 pd.read_csv()
 pd.crosstab()
 pd.value_counts().to_dict
+<<<<<<< HEAD
 
 import pandas_profiling as pdp
 pdp.ProfileReport(df)
@@ -16,7 +17,8 @@ for column in columns:
   column = df_categorical[column].value_counts()
   lst.append(column)
   print("{column} \n")
-
+=======
+>>>>>>> cd3dda03879bf3a61ce1675ebb4b5d098d8755d7
 
 # Get values which are not common in both train and test dataframe
 filter1=test_df["goods_name"].isin(train_df["goods_name"])
