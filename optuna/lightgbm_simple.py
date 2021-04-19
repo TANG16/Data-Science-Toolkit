c707b00f8a66ee4d5f21ba09f2ac62248e69c8d0
dsuke <daisuke0582@gmail.com>
2021-04-19 09:32:46 +0900

    change val name of optuna lightgbm

--- optuna/lightgbm_simple.py
+++ optuna/lightgbm_simple.py
@@ -16,8 +16,7 @@ import optuna
 # FYI: Objective functions can take additional arguments
 # (https://optuna.readthedocs.io/en/stable/faq.html#objective-func-additional-args).
 def objective(trial):
-    data, target = sklearn.datasets.load_breast_cancer(return_X_y=True)
-    train_x, valid_x, train_y, valid_y = train_test_split(data, target, test_size=0.25)
+    train_x, valid_x, train_y, valid_y = train_test_split(X, y, test_size=0.25)
     dtrain = lgb.Dataset(train_x, label=train_y)
 
     param = {
